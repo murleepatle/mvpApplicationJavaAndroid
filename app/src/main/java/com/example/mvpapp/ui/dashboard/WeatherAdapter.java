f01623236904df1672c1b847c283bec7a798513f
murlipatle <murlee.pat@gmail.com>
2021-11-03 18:17:26 +0530

    Search UI updated

--- app/src/main/java/com/example/mvpapp/ui/dashboard/WeatherAdapter.java
+++ app/src/main/java/com/example/mvpapp/ui/dashboard/WeatherAdapter.java
@@ -1,8 +1,13 @@
 package com.example.mvpapp.ui.dashboard;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,10 +16,16 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.mvpapp.R;
 import com.example.mvpapp.data.model.WeatherDataResponse;
 
+import java.io.InputStream;
+import java.net.URL;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class WeatherAdapter extends RecyclerView.Adapter<WeatherAdapter.ViewHolder>{
     private final List<WeatherDataResponse> weatherDataResponses;
+    ExecutorService executor =  Executors.newSingleThreadExecutor();
+    Handler handler = new Handler(Looper.getMainLooper());
 
     public WeatherAdapter(List<WeatherDataResponse> weatherDataResponses) {
         this.weatherDataResponses = weatherDataResponses;
@@ -23,7 +34,7 @@ public class WeatherAdapter extends RecyclerView.Adapter<WeatherAdapter.ViewHold
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
-        View listItem= layoutInflater.inflate(R.layout.list_item_post_office, parent, false);
+        View listItem= layoutInflater.inflate(R.layout.list_item_weather, parent, false);
         return new ViewHolder(listItem);
     }  
   
@@ -31,10 +42,23 @@ public class WeatherAdapter extends RecyclerView.Adapter<WeatherAdapter.ViewHold
     public void onBindViewHolder(ViewHolder holder, int position) {  
         final WeatherDataResponse weatherDataResponse = weatherDataResponses.get(position);
         holder.textViewLocation.setText(weatherDataResponse.getLocation().getName());
-        holder.textViewType.setText(weatherDataResponse.getCurrent().getCondition().getText());
-        holder.textViewTemp.setText((weatherDataResponse.getCurrent().getTempC()+"/°C"));
+        holder.textViewSubLoc.setText(weatherDataResponse.getLocation().getRegion());
+        holder.textViewType.setText((weatherDataResponse.getCurrent().getCondition().getText()+" | Feel like "+weatherDataResponse.getCurrent().getFeelslikeC()));
+        holder.textViewTemp.setText((weatherDataResponse.getCurrent().getTempC()+" /°C"));
         holder.textViewHumidity.setText((holder.itemView.getContext().getString(R.string.humidity) +(weatherDataResponse.getCurrent().getHumidity().toString())));
         holder.textViewLastUpdated.setText((holder.itemView.getContext().getString(R.string.last_update) + weatherDataResponse.getCurrent().getLastUpdated()));
+
+        executor.execute(() -> {
+            try {
+                Bitmap image;
+                InputStream inputStream = new URL("https:"+weatherDataResponse.getCurrent().getCondition().getIcon()).openStream();
+                image = BitmapFactory.decodeStream(inputStream);
+                handler.post(() -> holder.imageViewWeather.setImageBitmap(image));
+            } catch ( Exception e) {
+                e.printStackTrace();
+            }
+        });
+
 
     }  
   
@@ -51,14 +75,17 @@ public class WeatherAdapter extends RecyclerView.Adapter<WeatherAdapter.ViewHold
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {  
-        public TextView textViewLocation,textViewType,textViewTemp,textViewHumidity,textViewLastUpdated;
+        public TextView textViewLocation,textViewType,textViewTemp,textViewSubLoc,textViewHumidity,textViewLastUpdated;
+        public ImageView imageViewWeather;
         public ViewHolder(View itemView) {
             super(itemView);  
             this.textViewLocation = (TextView) itemView.findViewById(R.id.textViewLocation);
+            this.textViewSubLoc = (TextView) itemView.findViewById(R.id.textViewSubLoc);
             this.textViewType = (TextView) itemView.findViewById(R.id.textViewType);
             this.textViewTemp = (TextView) itemView.findViewById(R.id.textViewTemp);
             this.textViewHumidity = (TextView) itemView.findViewById(R.id.textViewHumidity);
             this.textViewLastUpdated = (TextView) itemView.findViewById(R.id.textViewLastUpdated);
+            this.imageViewWeather =  itemView.findViewById(R.id.imageViewWeather);
         }
     }  
 }  
